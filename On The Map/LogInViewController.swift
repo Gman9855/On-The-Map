dca858cf196afe78037927e6157905bb76923ae6
Gershy Lev <gbay9855@yahoo.com>
2015-05-23 00:15:35 -0700

    Implemented log-in logic for udacity email/password

--- On The Map/LogInViewController.swift
+++ On The Map/LogInViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    var userID: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,14 +32,34 @@ class ViewController: UIViewController {
             request.HTTPMethod = "POST"
             request.addValue("application/json", forHTTPHeaderField: "Accept")
             request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.HTTPBody = "{\"udacity\": {\"username\": \(emailTextField.text), \"password\": \(passwordTextField.text)}}".dataUsingEncoding(NSUTF8StringEncoding)
+            request.HTTPBody = "{\"udacity\": {\"username\": \"\(emailTextField.text)\", \"password\": \"\(passwordTextField.text)\"}}".dataUsingEncoding(NSUTF8StringEncoding)
             let session = NSURLSession.sharedSession()
             let task = session.dataTaskWithRequest(request) { data, response, error in
                 if error != nil { // Handle error…
+                    if error.code == -1009 {
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            var alert = UIAlertView(title: "No network connection", message: "Please connect to a network and try again", delegate: self, cancelButtonTitle: "Okay")
+                            alert.show()
+                        })
+                    }
                     return
                 }
+                
                 let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5)) /* subset response data! */
-                println(NSString(data: newData, encoding: NSUTF8StringEncoding))
+                var parsingError: NSError? = nil
+                let parsedResult = NSJSONSerialization.JSONObjectWithData(newData, options: NSJSONReadingOptions.AllowFragments, error: &parsingError) as! NSDictionary
+                if let userID = parsedResult.valueForKeyPath("account.key") as? String {
+                    self.userID = userID
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        self.performSegueWithIdentifier("segueToTabBar", sender: self)
+                    })
+                }
+                if let error = parsedResult["error"] as? String {
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        var alert = UIAlertView(title: nil, message: error, delegate: self, cancelButtonTitle: "Okay")
+                        alert.show()
+                    })
+                }
             }
             task.resume()
         }
@@ -47,5 +68,11 @@ class ViewController: UIViewController {
     @IBAction func signUpButtonTapped(sender: UIButton) {
         UIApplication.sharedApplication().openURL(NSURL(string: "https://www.udacity.com/account/auth#!/signup")!)
     }
+    
+//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+//        if segue.identifier == "segueToTabBar" {
+//            let mapVC = segue.destinationViewController as! MapViewController
+//        }
+//    }
 }
 
