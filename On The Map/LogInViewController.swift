ad2cc39cc796a65a611d0e68f0c7b94fbe989beb
Gershy Lev <gbay9855@yahoo.com>
2015-06-15 11:23:29 -0700

    Replace facebook/udacity log in methods with NetworkHandler call

--- On The Map/LogInViewController.swift
+++ On The Map/LogInViewController.swift
@@ -43,80 +43,47 @@ class ViewController: UIViewController {
     
     @IBAction func loginButtonTapped(sender: UIButton) {
         if emailTextField.text.isEmpty || passwordTextField.text.isEmpty {
-            //make label and alert user
+            var alert = UIAlertView(title: nil, message: "Please enter an email address and password", delegate: self, cancelButtonTitle: "Okay")
+            alert.show()
         } else {
-            let request = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/session")!)
-            request.HTTPMethod = "POST"
-            request.addValue("application/json", forHTTPHeaderField: "Accept")
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.HTTPBody = "{\"udacity\": {\"username\": \"\(emailTextField.text)\", \"password\": \"\(passwordTextField.text)\"}}".dataUsingEncoding(NSUTF8StringEncoding)
-            let session = NSURLSession.sharedSession()
-            let task = session.dataTaskWithRequest(request) { data, response, error in
+            NetworkHandler().loginWithUdacity(emailTextField.text, password: passwordTextField.text, completion: { (success, error) -> () in
                 if error != nil { // Handle error…
-                    if error.code == -1009 {
+                    if error!.code == -1009 {
                         dispatch_async(dispatch_get_main_queue(), { () -> Void in
                             var alert = UIAlertView(title: "No network connection", message: "Please connect to a network and try again", delegate: self, cancelButtonTitle: "Okay")
                             alert.show()
                         })
                     }
-                    return
-                }
-                
-                let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5)) /* subset response data! */
-                var parsingError: NSError? = nil
-                let parsedResult = NSJSONSerialization.JSONObjectWithData(newData, options: NSJSONReadingOptions.AllowFragments, error: &parsingError) as! NSDictionary
-                if let userID = parsedResult.valueForKeyPath("account.key") as? String {
-                    self.userID = userID
-                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                        self.performSegueWithIdentifier("segueToTabBar", sender: self)
-                    })
-                }
-                if let error = parsedResult["error"] as? String {
-                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                        var alert = UIAlertView(title: nil, message: error, delegate: self, cancelButtonTitle: "Okay")
-                        alert.show()
-                    })
-                }
-            }
-            task.resume()
-        }
-    }
-
-    @IBAction func facebookLoginButtonTapped(sender: UIButton) {
-        
-        var login = FBSDKLoginManager()
-        login.logInWithReadPermissions(nil, handler: { (result: FBSDKLoginManagerLoginResult!, error: NSError!) -> Void in
-            var fbAccessToken = FBSDKAccessToken.currentAccessToken().tokenString
-            let request = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/session")!)
-            request.HTTPMethod = "POST"
-            request.addValue("application/json", forHTTPHeaderField: "Accept")
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.HTTPBody = "{\"facebook_mobile\": {\"access_token\": \"\(fbAccessToken)\"}}".dataUsingEncoding(NSUTF8StringEncoding)
-            let session = NSURLSession.sharedSession()
-            let task = session.dataTaskWithRequest(request) { data, response, error in
-                if error != nil {
-                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                        var alert = UIAlertView(title: nil, message: "Something went wrong.", delegate: self, cancelButtonTitle: "Okay")
-                        alert.show()
-                        return
-                    })
-                }
-                let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5)) /* subset response data! */
-                var parsingError: NSError? = nil
-                var parsedResult = NSJSONSerialization.JSONObjectWithData(newData, options: NSJSONReadingOptions.AllowFragments, error: &parsingError) as! NSDictionary
-                if parsedResult["account"] == nil {
-                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                        var alert = UIAlertView(title: "Error logging in", message: "Looks like you need to connect your Facebook account to udacity.", delegate: self, cancelButtonTitle: "Okay")
-                        alert.show()
-                    })
+                    if error!.code == -1010 {
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            var alert = UIAlertView(title: nil, message: error?.domain, delegate: self, cancelButtonTitle: "Okay")
+                            alert.show()
+                        })
+                    }
                 } else {
                     dispatch_async(dispatch_get_main_queue(), { () -> Void in
                         self.performSegueWithIdentifier("segueToTabBar", sender: self)
                     })
                 }
+            })
+        }
+    }
+
+    @IBAction func facebookLoginButtonTapped(sender: UIButton) {
+        NetworkHandler().loginWithFacebook { (success, error) -> () in
+            if error != nil {
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    var alert = UIAlertView(title: nil, message: error?.domain, delegate: self, cancelButtonTitle: "Okay")
+                    alert.show()
+                    return
+                })
             }
-            task.resume()
-        })
+            if success {
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    self.performSegueWithIdentifier("segueToTabBar", sender: self)
+                })
+            }
+        }
     }
     
     @IBAction func signUpButtonTapped(sender: UIButton) {
