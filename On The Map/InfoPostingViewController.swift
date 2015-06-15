7effc6d045f3e1db577be1118f9453e4da77466f
Gershy Lev <gbay9855@yahoo.com>
2015-06-14 22:15:33 -0700

    Add MBProgressHUD, InfoPostingVC, and Bridging header

---
+++ On The Map/InfoPostingViewController.swift
@@ -0,0 +1,13 @@
+//
+//  InfoPostingViewController.swift
+//  On The Map
+//
+//  Created by Gershy Lev on 6/2/15.
+//  Copyright (c) 2015 Gershy Lev. All rights reserved.
+//
+
+import UIKit
+
+class InfoPostingViewController: UIViewController {
+    
+}
---
+++ On The Map/NetworkHandler.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkLoginManager.swift
+//  
+//
+//  Created by Gershy Lev on 6/6/15.
+//
+//
+
+import Foundation
